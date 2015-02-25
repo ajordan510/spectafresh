7b45186f43540eaa1942263e9242aff15baf9fa7
Alex Jordan <alex.jordan.j@gmail.com>
2015-02-25 02:26:57 +0000

    first heroku push

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(email: 'alex.jordan.j@gmail.com', description: 'Interpretive dancer by nature, comedian by heart')
+User.create!(email: 'jenjones32@gmail.com', description: '100% wholesome content.  Twerking is gross')
